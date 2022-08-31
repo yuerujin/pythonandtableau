67d6cc19d10082778894faaf999b7df0979cb7fc
yuerujin <112587619+yuerujin@users.noreply.github.com>
2022-08-31 17:07:52 -0400

    ValueInc_Sales.py
    This is my code for the project Value Inc.

---
+++ valueinc_sales.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 14:55:03 2022
+
+@author: yuerujin
+"""
+
+import pandas as pd
+
+# file_name = pd.read_csv('file.csv') <---- format of read_csv
+
+data = pd.read_csv('transaction.csv')
